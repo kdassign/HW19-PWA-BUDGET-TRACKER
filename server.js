1a763946b988a427af774cf82d6b40495eb20e95
Kelsey Duru <kdassignments@gmail.com>
2021-12-07 12:31:30 -0800

    deployed to heroku and now I'm messing with the server.js file in order to get everything to work

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -21,8 +21,9 @@ mongoose.connect
 {
   useNewUrlParser: true,
   useFindAndModify: false,
-},
-);
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 // routes
 app.use(require("./routes/api.js"));
